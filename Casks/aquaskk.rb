70b681cc50af42d5324c4adae99c6f3dc543bb59
aereal <aereal@aereal.org>
2014-01-12 00:05:38 +0900

    Uninstall support

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -4,5 +4,5 @@ class Aquaskk < Cask
   version '4.2'
   sha1 '6a7110daea819acd7650499a009c0e41fd882355'
   install 'AquaSKK.pkg'
-  pkgutil 'jp.surceforge.inputmethod.aquaskk.pkg'
+  uninstall :pkgutil => 'jp.surceforge.inputmethod.aquaskk.pkg'
 end

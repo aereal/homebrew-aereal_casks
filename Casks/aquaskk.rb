8e9d171e6cb22d86d4760e305ccbba6a36c3d330
aereal <aereal@aereal.org>
2014-01-11 23:39:54 +0900

    Add AquaSKK 4.2

---
+++ Casks/aquaskk.rb
@@ -0,0 +1,8 @@
+class Aquaskk < Cask
+  url 'http://jaist.dl.sourceforge.jp/aquaskk/58814/AquaSKK-4.2.dmg'
+  homepage 'http://aquaskk.sourceforge.jp/'
+  version '4.2'
+  sha1 '6a7110daea819acd7650499a009c0e41fd882355'
+  install 'AquaSKK.pkg'
+  pkgutil 'jp.surceforge.inputmethod.aquaskk.pkg'
+end

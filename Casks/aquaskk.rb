77ccd20615833e8ace98d9684fa98abdac476b58
Takeru Naito <takeru.naito@gmail.com>
2015-11-09 21:32:07 +0900

    Upgrade to 4.3.2
    Descriptions are:
    
    - https://github.com/codefirst/aquaskk/releases/tag/4.3.2
    - https://github.com/codefirst/aquaskk/releases/tag/4.3.1
    - https://github.com/codefirst/aquaskk/releases/tag/4.3.0

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'aquaskk' do
-  version '4.2.7'
+  version '4.3.2'
   url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.dmg"
   name 'AquaSKK'
   homepage 'https://github.com/codefirst/aquaskk'
-  sha256 'ce0e4dab61b68ce832b700ec061f364fcddeb13fd64e84b7fb6e79aa01dd60a1'
+  sha256 '1294b4e02b563410796737c86467cd321f87cc85e359a132c26ea0a2f1a344fe'
   license :gpl # GPL v2
   pkg 'AquaSKK.pkg'
   uninstall :pkgutil => 'jp.sourceforge.inputmethod.aquaskk.pkg'

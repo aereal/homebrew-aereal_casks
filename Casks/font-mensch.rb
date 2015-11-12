05d2e524f4a6606ab862680526c98141a0a45f5a
aereal <aereal@aereal.org>
2015-11-12 14:32:10 +0900

    + font-mensch

---
+++ Casks/font-mensch.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-mensch' do
+  version '2.0'
+  sha256 'ee174aee776b3b34762e6792d4cd470e86e13a504de247dc35de9835924d932f'
+
+  url 'http://robey.lag.net/downloads/mensch2.otf'
+  name 'font-mensch'
+  homepage 'http://robey.lag.net/2012/08/22/mensch-2.html'
+  license :oss
+
+  font 'mensch2.otf'
+end
